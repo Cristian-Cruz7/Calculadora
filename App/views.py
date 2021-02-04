6dfdf08bd99d0494feadf8333baeed7ad0c234c8
Cristian Cruz <cristian.camilo.cruz@hotmail.com>
2021-02-04 11:48:42 -0500

    Funcionamiento básico

--- App/views.py
+++ App/views.py
@@ -1,17 +1,1 @@
 from flask import Flask, render_template, redirect
-
-@app.route("/api/suma")
-def Suma():
-    pass    
-
-@app.route("/api/resta")
-def Resta():
-    pass
-
-@app.route("/api/multiplicacion")
-def Multiplicacion():
-    pass
-
-@app.route("/api/division")
-def Division():
-    pass
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, render_template, redirect
+from flask import render_template, redirect
 
-from . import views
+from app import create_app
+from app.views import *
 
-app =Flask(__name__)
+app = create_app()
 
 @app.route('/')
 def index():
@@ -10,9 +11,29 @@ def index():
 
 @app.route('/api')
 def api():
+   return render_template('Home.html')
+
+@app.route('/api/otraApp')
+def api_otraApp():
+   return render_template('otraApp.html')
+
+@app.route('/api/calc')
+def api_calc():
    return render_template('calculadora.html')
 
 
+@app.route("/api/calc/Suma")
+def api_calc_Suma():
+    pass    
 
+@app.route("/api/calc/Resta")
+def api_calc_Resta():
+    pass
 
+@app.route("/api/calc/Multiplicacion")
+def api_calc_Multiplicacion():
+    pass
 
+@app.route("/api/calc/Division")
+def api_calc_Division():
+    pass
