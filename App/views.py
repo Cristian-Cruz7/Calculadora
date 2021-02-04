222e905f6aafe8a178ede096e1e87f5fb46ebb05
Cristian Cruz <cristian.camilo.cruz@hotmail.com>
2021-02-03 22:38:53 -0500

    Primeros Enlaces Calculadora

---
+++ App/views.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, redirect
+
+@app.route("/api/suma")
+def Suma():
+    pass    
+
+@app.route("/api/resta")
+def Resta():
+    pass
+
+@app.route("/api/multiplicacion")
+def Multiplicacion():
+    pass
+
+@app.route("/api/division")
+def Division():
+    pass
--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect
 
-app =Flask(__Calculadora__)
+from . import views
+
+app =Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect('/api')
+
+@app.route('/api')
+def api():
+   return render_template('calculadora.html')
+
+
+
+
+
