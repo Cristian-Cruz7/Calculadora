677aa3538458b67245cfe879a6af88812741d0a9
Cristian Cruz <cristian.camilo.cruz@hotmail.com>
2021-02-08 00:27:06 -0500

    Final calculadora js

--- App/static/funciones.js
+++ App/static/funciones.js
@@ -1,62 +1,118 @@
-function init(){
-    // variables
-    var resultado = document.getElementsById("resultado");
-    var sum = document.getElementsById("sum");
-    var res = document.getElementsById("res");
-    var mul = document.getElementsById("mul");
-    var div = document.getElementsById("div");
-    var siete = document.getElementsById("siete");
-    var ocho = document.getElementsById("ocho");
-    var nueve = document.getElementsById("nueve");
-    var del = document.getElementsByClassName("del");
-    var seis = document.getElementsByClassName("seis");
-    var cinco = document.getElementsByClassName("cinco");
-    var cuatro = document.getElementsByClassName("cuatro");
-    var cero = document.getElementsByClassName("cero");
-    var uno = document.getElementsByClassName("uno");
-    var dos = document.getElementsByClassName("dos");
-    var tres = document.getElementById("tres");
-    var igual = document.getElementsByClassName("igual");
+var valorUno;
+var valorDos;
+var operacion; 
 
-    //eventos
-    uno.onclick = function(e){  //al hacer clic en el boton, la funcion recibe el evento "e", luego a la variable
-        resultado.textContent = resultado.textContent + "1"  //resultado, le da el valor que tiene almacenado, pero
+function init(){
+    //variables
+
+    var resultado = document.getElementById("resultado");
+    var uno = document.getElementById("uno");
+    var dos = document.getElementById("dos");
+    var tres = document.getElementById("tres");
+    var cuatro = document.getElementById("cuatro");
+    var cinco = document.getElementById("cinco");
+    var seis = document.getElementById("seis");
+    var siete = document.getElementById("siete");
+    var ocho = document.getElementById("ocho");
+    var nueve = document.getElementById("nueve");
+    var cero = document.getElementById("cero");
+    var sum = document.getElementById("sum");
+    var res = document.getElementById("res");
+    var mul = document.getElementById("mul");
+    var div = document.getElementById("div");
+    var del = document.getElementById("del");
+    var igual = document.getElementById("igual");
+
+        //eventos 
+        uno.onclick = function(e){  //al hacer clic en el boton, la funcion recibe el evento "e", luego a la variable
+        resultado.textContent = resultado.textContent + "1";  //resultado, le da el valor que tiene almacenado, pero
     }                                               // le suma un uno a los datos ya almacenados
 
     dos.onclick = function(e){
-        resultado.textContent = resultado.textContent + "2"
+        resultado.textContent = resultado.textContent + "2";
     }
 
     tres.onclick = function(e){
-        resultado.textContent = resultado.textContent + "3"
+        resultado.textContent = resultado.textContent + "3";
     }
 
     cuatro.onclick = function(e){
-        resultado.textContent = resultado.textContent + "4"
+        resultado.textContent = resultado.textContent + "4";
     }
 
     cinco.onclick = function(e){
-        resultado.textContent = resultado.textContent + "5"
+        resultado.textContent = resultado.textContent + "5";
     }
 
     seis.onclick = function(e){
-        resultado.textContent = resultado.textContent + "6"
+        resultado.textContent = resultado.textContent + "6";
     }
 
     siete.onclick = function(e){
-        resultado.textContent = resultado.textContent + "7"
+        resultado.textContent = resultado.textContent + "7";
     }
 
     ocho.onclick = function(e){
-        resultado.textContent = resultado.textContent + "8"
+        resultado.textContent = resultado.textContent + "8";
     }
 
     nueve.onclick = function(e){
-        resultado.textContent = resultado.textContent + "9"
+        resultado.textContent = resultado.textContent + "9";
     }
 
     cero.onclick = function(e){
-        resultado.textContent = resultado.textContent + "0"
+        resultado.textContent = resultado.textContent + "0";
+    }
+
+    del.onclick = function(e){
+        resultado.textContent = "";
+        valorUno = 0;
+        valorDos = 0;
+        operacion = "";
+    }
+    sum.onclick = function(e){
+        valorUno = resultado.textContent;
+        operacion = "+";
+        resultado.textContent = "";
+    }
+    res.onclick = function(e){
+        valorUno = resultado.textContent;
+        operacion = "-";
+        resultado.textContent = "";
+    }
+    mul.onclick = function(e){
+        valorUno = resultado.textContent;
+        operacion = "*";
+        resultado.textContent = "";
+    }
+    div.onclick = function(e){
+        valorUno = resultado.textContent;
+        operacion = "/";
+        resultado.textContent = "";
+    }
+    igual.onclick = function(e){
+        valorDos = resultado.textContent;
+        var respuesta = 0;
+        switch(operacion){
+          case "+":
+            respuesta = parseFloat(valorUno) + parseFloat(valorDos); /* parsefloat conviertstring en flotantes*/
+            break;
+          case "-":
+            respuesta = parseFloat(valorUno) - parseFloat(valorDos);
+            break;
+          case "*":
+            respuesta = parseFloat(valorUno) * parseFloat(valorDos);
+            break;
+          case "/":
+            respuesta = parseFloat(valorUno) / parseFloat(valorDos);
+            break;
+        }
+        resultado.textContent = "";
+        valorUno = 0;
+        valorDos = 0;
+        operacion = "";
+        resultado.textContent = respuesta;
+
     }
 
 }
